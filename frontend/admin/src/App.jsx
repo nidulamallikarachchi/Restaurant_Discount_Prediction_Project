21f6ca1f4f08fd347b9f18f94a9f1fcc480ed632
Nidula Mallikarachchi <nidulamallikarachchi@gmail.com>
2025-05-13 14:43:39 +1000

    Tailwind CSS Configuration For Admin Dashboard

--- frontend/admin/src/App.jsx
+++ frontend/admin/src/App.jsx
@@ -3,8 +3,8 @@ import DiscountManager from './components/DiscountManager';
 function App() {
   return (
     <div>
-      <h1>Admin Dashboard</h1>
-      <DiscountManager />
+        <h1 className="text-3xl font-bold text-center text-blue-600">Admin Dashboard</h1>
+        <DiscountManager/>
     </div>
   );
 }
